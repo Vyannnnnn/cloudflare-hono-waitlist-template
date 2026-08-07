c659d0477ff5cf2cc4520ca5a41c997fcb98d4c8
Vyannnnnn <fian88518@gmail.com>
2026-08-07 10:19:42 +0700

    Adding a Hero Section, Email Input, and Cards with daisyUI

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,22 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   srcDir: "./src/client",
 
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:1234"
+        }
+      }
+    }
   },
+
+  integrations: [react()],
 });
